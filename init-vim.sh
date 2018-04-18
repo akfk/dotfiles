bb0fb9122c17a208e9261d04e8544c3835743d63
fukuhara <fukuhara@fukuhara-linux.westlab>
2018-04-18 17:05:25 +0900

    add init-vim.sh

---
+++ init-vim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#create .vim dir in PWD
+if [ ! -e $PWD/.vim ]; then
+  mkdir $PWD/.vim
+fi
+
+# create links
+echo "ln -s $PWD/.vim $HOME"
+ln -s $PWD/.vim $HOME
+echo "ln -s $PWD/.vimrc $HOME"
+ln -s $PWD/.vimrc $HOME
+
+# Install NeoBundle
+echo "Install NeoBundle"
+git clone https://github/Shougo/neobundle.vim $PWD/.vim/bundle/neobundle.vim
+
+echo "Please run vim."
